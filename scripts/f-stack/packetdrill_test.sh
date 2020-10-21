95fa0296d70bb01d32ddfb65e2e785659a05db8b
zouyonghao <zouyonghao1994@163.com>
2020-10-21 22:00:47 +0800

    Update report

--- scripts/f-stack/packetdrill_test.sh
+++ scripts/f-stack/packetdrill_test.sh
@@ -6,5 +6,6 @@ sudo ../../packetdrill --local_ip=192.168.0.2 --remote_ip=192.0.2.1 --tolerance_
  --tun_id=0 $1
 
 echo -e "${RED}Testing with so file...${NC}"
+export ASAN_OPTIONS=abort_on_error=0:detect_leaks=0
 sudo ../../packetdrill --local_ip=192.168.0.2 --remote_ip=192.0.2.1 --so_filename=1 --tolerance_usecs=200000000 \
  --tun_id=0 $1
