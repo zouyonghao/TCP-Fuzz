62b58e67cb0fd3903201e9595958a5f9d0b4dec8
zouyonghao <yonghaoz1994@gmail.com>
2020-10-22 22:03:00 +0800

    Delete unused filename

--- scripts/tldk/fuzz_test_afl_slave4_old.sh
+++ scripts/tldk/fuzz_test_afl_slave4_old.sh
@@ -1,4 +1,4 @@
 /bin/AFLCplusplus -i input -o output -f fuzz_test_input4 -S fuzzer4 \
-../../fuzz_test --local_ip=192.168.1.4 --remote_ip=192.0.1.4 --so_filename=/home/$(USER)/Desktop/tldk-old/libtldk.so --tolerance_usecs=200000000 \
+../../fuzz_test --local_ip=192.168.1.4 --remote_ip=192.0.1.4 --so_filename=1 --tolerance_usecs=200000000 \
  --fuzz_file_name=fuzz_test_input4 \
  --tun_id=4
