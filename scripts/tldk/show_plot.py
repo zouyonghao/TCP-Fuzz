b037635629967038981a71ade85475adce0b46c0
zouyonghao <yonghaoz1994@gmail.com>
2020-11-02 10:59:20 +0800

    Update show_plot script

--- scripts/tldk/show_plot.py
+++ scripts/tldk/show_plot.py
@@ -6,7 +6,9 @@ from io import StringIO
 
 f = open('./plot-curve')
 csv = 'time,old_cov,new_cov\n'
+count = 0
 for line in f:
+    count += 1
     line_items = line.split(' ')
     # print(line_items)
     time = line_items[0]
@@ -15,28 +17,31 @@ for line in f:
 
     # old_cov = line_items[3][0:line_items[3].index('(')]
     # new_cov = line_items[12]
+    # csv += time + ',' + old_cov + ',' + '0' + '\n'
     csv += time + ',' + old_cov + ',' + new_cov + '\n'
-    # break
+    # if count > 1000:
+    #     break
 
-print(csv)
+# print(csv)
 data = pd.read_csv(StringIO(csv))
 
-for i in data:
-    print(i)
-    break
+# for i in data:
+#     print(i)
+#     break
 
 df = pd.DataFrame(data, columns = ['time', 'old_cov', 'new_cov'])
 
-print(df)
+# print(df)
 # Set the Date as Index
-# df['time'] = pd.to_datetime(df['time'])
+# df['time'] = pd.to_datetime(df['time'], unit='us')
+df['time'] = pd.to_timedelta(df['time'] - df['time'][0], unit='us')
 df.index = df['time']
 del df['time']
 
-df['old_cov'] = np.log10(df['old_cov'])
-df['new_cov'] = np.log10(df['new_cov'])
+# df['old_cov'] = np.log10(df['old_cov'])
+# df['new_cov'] = np.log10(df['new_cov'])
 
-print(df)
+# print(df)
 
 df.plot(figsize=(15, 6))
 plt.savefig('test.png')
