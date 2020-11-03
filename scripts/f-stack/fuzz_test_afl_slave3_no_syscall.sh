25f98acb92669737f587fe23c20cbf03a22b88cd
zouyonghao <yonghaoz1994@gmail.com>
2020-11-03 11:08:01 +0800

    Add no_packet scripts for f-stack

---
+++ scripts/f-stack/fuzz_test_afl_slave3_no_syscall.sh
@@ -0,0 +1,6 @@
+/bin/AFLCplusplus -i input -o output -f fuzz_test_input3 -S fuzzer3 \
+../../fuzz_test --local_ip=192.168.1.3 --remote_ip=192.0.1.3 --so_filename=1 --tolerance_usecs=200000000 \
+ --fuzz_file_name=fuzz_test_input3 \
+ --no_fuzz_syscall \
+ --tun_id=3 \
+ --no_fuzz_receive_packets_in_background
