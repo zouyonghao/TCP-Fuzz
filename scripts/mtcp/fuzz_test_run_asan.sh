d5ecbae77c2368a732678b354274d28fa4a2b3f3
zouyonghao <yonghaoz1994@gmail.com>
2020-12-28 11:30:08 +0800

    Add run_asan

---
+++ scripts/mtcp/fuzz_test_run_asan.sh
@@ -0,0 +1,6 @@
+sudo ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-6.0/bin/llvm-symbolizer ASAN_OPTIONS=detect_leaks=0:allocator_may_return_null=1 \
+   ../../fuzz_test\
+    --local_ip=192.168.0.2 --remote_ip=192.0.0.2 \
+    --so_filename=1 --tolerance_usecs=100000000 \
+    --tun_id=6 \
+    --fuzz_file_name=$1 --no_abort
